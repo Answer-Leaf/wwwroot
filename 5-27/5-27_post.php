df4d72a95754db024e5d399fdea5b9de79a1c58f
Answer <Answer@after.com>
2021-05-27 19:01:22 +0800

    表单提交

--- 5-27/5-27_post.php
+++ 5-27/5-27_post.php
@@ -8,12 +8,15 @@ $inp_u=$_POST['inp_u'];
 $inp_u_reg="/^[A-z]{6,}$/";
 if (!preg_match($inp_u_reg,$inp_u)){
     echo "用户名输入有误<br>";
+    die;
 }
 
 $inp_e=$_POST['inp_e'];
-$inp_e_reg="/^[A-z0-9]{5,20}@[A-z0-9]{2,9}\.[A-z]{2,6}$/";
-if (!preg_match($inp_e_reg,$inp_e)){
+//$inp_e_reg="/^[A-z0-9]{5,20}@[A-z0-9]{2,9}\.[A-z]{2,6}$/";
+//filter_var($inp_e,FILTER_VALIDATE_EMAIL);
+if (!filter_var($inp_e,FILTER_VALIDATE_EMAIL)){
     echo "邮箱输入有误<br>";
+    die;
 }
 
 
@@ -21,11 +24,13 @@ $inp_p=$_POST['inp_p'];
 $inp_p_reg="/^[0-9A-z]{6,16}$/";
 if (!preg_match($inp_p_reg,$inp_p)){
     echo "密码验证有误<br>";
+    die;
 }
 
 
 $inp_pT=$_POST['inp_pT'];
 if ($inp_pT!=$inp_p){
     echo "确认密码有误";
+    die;
 }
-
+echo "提交成功";
